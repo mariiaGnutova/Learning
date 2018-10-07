70caf7684645ba372e41fbee81555a367b27df43
mariiaGnutova <43730750+mariiaGnutova@users.noreply.github.com>
2018-10-07 22:14:33 +0200

    user wishes

--- Biography.java
+++ Biography.java
@@ -2,10 +2,33 @@ import javax.swing.*;
 
 public class Biography {
     public static void main(String[] args) {
-        String userName = JOptionPane.showInputDialog("Please enter your name:");
-        String userGender;
+        String name = "My name is ";
+        String nameEnd = ". ";
+        boolean addToBiography = false;
+        String userName;
+        StringBuilder question = new StringBuilder("What is your name?");
         do {
-            userGender = JOptionPane.showInputDialog("Please choose your gender: Enter 1 for female, 2 for male, or 3 if you don't identify yourself with a gender.");
+          userName = JOptionPane.showInputDialog(question);
+          addToBiography = checkInfo( userName, question.toString());
+        }
+        while (addToBiography & userName.length()<2);
+
+        if (!addToBiography){
+            name = "";
+            nameEnd = "";
+        }
+        String userGender;
+
+        String genderStart = " ";
+        String ageStart = " I'm only ";
+        String ageEnde = " years old. ";
+        String natianality = "My nationality is ";
+        String nationalityEnd = ".";
+        String job = " And my job is ";
+        String jobEnd = ".";
+        do {
+            question.replace(0, question.length(), "What is your gender: Enter 1 for female, 2 for male, or 3 if you don't identify yourself with a gender.");
+            userGender = JOptionPane.showInputDialog(question);
             if ((userGender.split("(\\d+)").length>0 || userGender.length()>1 || (Integer.parseInt(userGender)>3|| Integer.parseInt(userGender)<1))){
                 JOptionPane.showMessageDialog(
                         null,
@@ -16,17 +39,18 @@ public class Biography {
         }
         while (!(userGender.charAt(0) == '1'|| userGender.charAt(0)== '2'||userGender.charAt(0)== '3'));
         if (userGender.charAt(0)== '1'){
-            userGender ="I'm female";
+            userGender ="I'm female.";
         }
         else if(userGender.charAt(0)== '2'){
-            userGender ="I'm male";
+            userGender ="I'm male.";
         }
         else{
-            userGender ="I don't identify myself with a gender";
+            userGender ="I don't identify myself with a gender.";
         }
         String userAge;
         do {
-            userAge = JOptionPane.showInputDialog("How old are you?");
+            question.replace(0, question.length(),"How old are you?");
+            userAge = JOptionPane.showInputDialog(question);
              String [] ifOnlyDigets = userAge.split("(\\d+)");
             if (ifOnlyDigets.length>0){
                 JOptionPane.showMessageDialog(
@@ -34,25 +58,75 @@ public class Biography {
                         "please, enter only digits",
                         "title",
                         JOptionPane.INFORMATION_MESSAGE);
-
             }
-        }
-        while (userAge.split("(\\d+)").length>0);
+        } while (userAge.split("(\\d+)").length>0);
 
-        String userNationality = JOptionPane.showInputDialog("What nationality are you?");
-        String userJob = JOptionPane.showInputDialog("What is your job?");
-        StringBuilder myBiography = new StringBuilder("My name is ");
+
+        question.replace(0, question.length(),"What nationality are you?");
+
+        String userNationality;
+        addToBiography = false;
+        do {
+            userNationality = JOptionPane.showInputDialog(question);
+            addToBiography = checkInfo( userNationality, question.toString());
+        }
+        while (addToBiography & userName.length()<2);
+
+        if (!addToBiography){
+            nationalityEnd = "";
+            natianality = "";
+        }
+
+        question.replace(0, question.length(),"What is your job?");
+        addToBiography = false;
+        String userJob;
+        do {
+            userJob = JOptionPane.showInputDialog(question);
+            addToBiography = checkInfo( userJob, question.toString());
+        }
+        while (addToBiography & userName.length()<2);
+
+        if (!addToBiography){
+            job = "";
+            jobEnd = "";
+        }
+
+       // question.replace(0, question.length(),name);
+        StringBuilder myBiography = new StringBuilder();
+        myBiography.append(name);
         myBiography.append(userName);
-        myBiography.append(". ");
+        myBiography.append(nameEnd);
+        myBiography.append(genderStart);
         myBiography.append(userGender);
-        myBiography.append(". Only ");
+        myBiography.append(ageStart);
         myBiography.append(userAge);
-        myBiography.append(" years old. My nationality is ");
+        myBiography.append(ageEnde);
+        myBiography.append(natianality);
         myBiography.append(userNationality);
-        myBiography.append(" and my job is ");
+        myBiography.append(nationalityEnd);
+        myBiography.append(job);
         myBiography.append(userJob);
-        myBiography.append(".");
+        myBiography.append(jobEnd);
+        if (myBiography.length() ==0 ){
+            JOptionPane.showMessageDialog(
+                    null,
+                    "It's sade, that you don't want to sheare information about you",
+                    ":(",
+                    JOptionPane.INFORMATION_MESSAGE);
+        }
         System.out.println(myBiography);
+        }
+
+        public static boolean checkInfo (String yourInfo, String wantToKnow){
+            boolean addToBiography = true;
+            if (yourInfo.length() <2){
+                addToBiography = true;
+            int reply = JOptionPane.showConfirmDialog(null, "Do you want to add information about your? " + wantToKnow, yourInfo, JOptionPane.YES_NO_OPTION);
+            if (!(reply == JOptionPane.YES_OPTION)){
+                addToBiography = false;
+             }
+            }
+        return addToBiography;
         }
     }
 
